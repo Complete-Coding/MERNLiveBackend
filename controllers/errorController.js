da1ef0108b9bc24683e46d3909da1cc00f5e70f7
Prashant Jain <prashant@knowledgegate.in>
2024-12-04 07:29:52 +0530

    Initial Project Checkpoint

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,3 @@
 exports.get404 = (req, res, next) => {
-  res.statusCode = 404;
-  res.render("404", {
-    pageTitle: "Page Not Found",
-    isLoggedIn: req.session.isLoggedIn,
-    user: req.session.user,
-  });
+  res.status(404).send("Page not found");
 };
